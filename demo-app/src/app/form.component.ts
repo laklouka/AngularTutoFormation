0a1766881de16a18a92ee355812e51e97655bf51
ihab abadi <ihab@utopios.net>
2021-05-11 20:26:10 +0200

    correction directive

--- demo-app/src/app/form.component.ts
+++ demo-app/src/app/form.component.ts
@@ -8,20 +8,26 @@ import { PersonInterface } from "./interfaces/PersonInterface";
 export class FormPerson {
 
     @Output() addPerson = new EventEmitter<PersonInterface>()
-
+    error : boolean = false
     firstName:String
     lastName:String
     phone:String
 
     confirmClick() {
-        const person : PersonInterface = {
-            firstName : this.firstName,
-            lastName : this.lastName,
-            phone : this.phone,
+        if(this.firstName != null && this.lastName != null && this.phone != null) {
+            const person : PersonInterface = {
+                firstName : this.firstName,
+                lastName : this.lastName,
+                phone : this.phone,
+            }
+            this.firstName =""
+            this.lastName=""
+            this.phone=""
+            this.addPerson.emit(person)
+            this.error = false
         }
-        this.firstName =""
-        this.lastName=""
-        this.phone=""
-        this.addPerson.emit(person)
+        else {
+            this.error = true            
+        }
     }
 }
