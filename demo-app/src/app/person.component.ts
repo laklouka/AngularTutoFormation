f6596deded73a4fdeb4c7faf330b78a939825463
ihab abadi <ihab@utopios.net>
2021-05-12 19:19:47 +0200

    suite correction exemple person component

--- demo-app/src/app/person.component.ts
+++ demo-app/src/app/person.component.ts
@@ -1,11 +1,14 @@
 import { Component, Input } from "@angular/core";
+import { PersonInterface } from "./interfaces/PersonInterface";
 
 @Component({
     selector : 'person-component',
-    template : "<div>Nom : {{lastName}}, prénom : {{firstName}}, téléphone : {{phone}}</div>"
+    template : "<div>Nom : {{person.lastName}}, prénom : {{person.firstName}}, téléphone : {{person.phone}}</div>"
 })
 export class PersonComponent {
-    @Input()firstName : String
-    @Input()lastName : String
-    @Input()phone : String
+    // @Input()firstName : String
+    // @Input()lastName : String
+    // @Input()phone : String
+
+    @Input() person : PersonInterface
 }
