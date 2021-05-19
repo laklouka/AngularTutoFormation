82866fc3d70ad213039ad51798a60f46b0f12483
ihab abadi <ihab@utopios.net>
2021-05-19 20:21:24 +0200

    suite correction panier

--- demo-module/src/product-module/components/product-component/product.component.ts
+++ demo-module/src/product-module/components/product-component/product.component.ts
@@ -1,9 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { ProductInterface } from "src/product-module/interfaces/ProductInterface";
+import { ProductsService } from "src/product-module/services/products.service";
 
 @Component({
     selector : 'product',
     templateUrl : "./product.component.html"
 })
-export class ProductComponent {
-    
+export class ProductComponent implements OnInit {
+    product : ProductInterface
+
+    constructor(private productService:ProductsService, private route:ActivatedRoute) {
+
+    }
+    ngOnInit(): void {
+        this.route.params.subscribe(pars => {
+            const id = pars["id"]
+            if(id != undefined) {
+                this.product = this.productService.products.find(p => p.id == id)
+            }
+        })
+    }
 }
