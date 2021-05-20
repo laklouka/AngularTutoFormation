f4888b2f26bf45133635c8e04c4742112d01966c
ihab abadi <ihab@utopios.net>
2021-05-20 18:58:46 +0200

    suite route childredn

--- demo-module/src/product-module/components/comment-component/comment.component.ts
+++ demo-module/src/product-module/components/comment-component/comment.component.ts
@@ -1,9 +1,19 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
     selector : 'comment',
     templateUrl :"./comment.component.html"
 })
-export class CommentComponent {
+export class CommentComponent implements OnInit {
+    productId : number
 
+    constructor(private route:ActivatedRoute) {
+    }
+    ngOnInit(): void {
+       
+        this.route.parent.params.subscribe(pars => {
+            this.productId = pars["id"]
+        })
+    }
 }
